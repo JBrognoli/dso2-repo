6c12bbafb3b61e5bd0908d9bb92ca8f2b1e82ccd
JBrognoli <brognolijoao@hotmail.com>
2019-09-13 21:59:50 -0300

    feat(project): introduce carousel data structure

---
+++ frontend/src/components/BaseCard.vue
@@ -0,0 +1,27 @@
+<template>
+<v-card>
+    <v-card-title>
+        Produto 3
+    </v-card-title> 
+    <v-card-text>
+        
+    </v-card-text>
+    <v-card-actions>
+        <v-btn>Comprar agora!</v-btn>
+    </v-card-actions>
+</v-card>
+</template>
+
+<script>
+  export default {
+    name: "BaseCard",
+    data() {
+      return {};
+    },
+    methods: {}
+  };
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
-  <v-container class="pt-0">
+  <v-container  class="pt-0">
     <v-layout class="align-center justify-center">
       <Carousel></Carousel>
+    </v-layout>
+    <v-layout class="mt-5">
+      <BaseCard></BaseCard>
     </v-layout>
   </v-container>
 </template>
 
 <script>
   import Carousel from '../components/Carousel'
+  import BaseCard from '../components/BaseCard'
 
   export default {
     components: {
-      Carousel
+      Carousel,
+      BaseCard
     },
     data: () => ({})
   };
